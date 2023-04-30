b0aa2759c3e5d0ba4cebff11bee25d60034efee2
Evgen Kyselgov <kyselgov@gmail.com>
2023-04-30 23:32:41 +0300

    add sin wave generator + test

---
+++ EEGCore/Processing/Generators/ISampleGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace EEGCore.Processing.Generators
+{
+    public interface ISampleGenerator
+    {
+        public void Generate(double[] samples, bool addGenerated = false);
+    }
+}
---
+++ EEGCoreTests/SinGeneratorTests.cs
@@ -0,0 +1,55 @@
+﻿using EEGCore.Processing.Generators;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace EEGCoreTests
+{
+    [TestClass]
+    public class SinGeneratorTests
+    {
+        [TestMethod]
+        public void Sinusoidal()
+        {
+            var period = (int)Math.Floor(Math.PI * 2 * 1000);
+            var length = period + 1;
+            var samples = new double[length];
+
+            var generator = new SinGenerator() { Amplitude = 200, Frequence = 1, SampleRate = period };
+            generator.Generate(samples, false);
+
+            Assert.AreEqual(Math.Abs(samples[0]), 0.0, 0.001);
+            Assert.AreEqual(Math.Abs(samples[length - 1]), 0.0, 0.001);
+
+            var vector = new DenseVector(samples);
+            var maxIndex = vector.MaximumIndex();
+            var minIndex = vector.MinimumIndex();
+
+            Assert.AreEqual(maxIndex, length / 4, 1);
+            Assert.AreEqual(minIndex, (length * 3) / 4, 1);
+            Assert.AreEqual(samples[maxIndex], -samples[minIndex], 0.001);
+        }
+
+        [TestMethod]
+        public void SinusoidalAdd()
+        {
+            var period = (int)Math.Floor(Math.PI * 2 * 1000);
+            var length = period + 1;
+            var mean = 10.0;
+            var samples = new double[length];
+            Array.Fill(samples, mean);
+
+            var generator = new SinGenerator() { Amplitude = 200, Frequence = 1, SampleRate = period };
+            generator.Generate(samples, true);
+
+            Assert.AreEqual(Math.Abs(samples[0]), mean, 0.001);
+            Assert.AreEqual(Math.Abs(samples[length - 1]), mean, 0.001);
+
+            var vector = new DenseVector(samples);
+            var maxIndex = vector.MaximumIndex();
+            var minIndex = vector.MinimumIndex();
+
+            Assert.AreEqual(maxIndex, length / 4, 1);
+            Assert.AreEqual(minIndex, (length * 3) / 4, 1);
+            Assert.AreEqual(samples[maxIndex] - mean, mean - samples[minIndex], 0.001);
+        }
+    }
+}
